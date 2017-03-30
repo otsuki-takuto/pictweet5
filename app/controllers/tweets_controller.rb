ff914178f5f331f729ac7856450259d63f571f43
大槻拓翔 <otakuto0521@gamil.com>
2017-03-30 13:41:16 +0900

    trippie

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,30 @@
+class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: :index
+
+  def index
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:oage]).per(5)
+  end
+
+  def new
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  def create
+      Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    end
+
+  private
+  def tweet_params
+    params.permit(:image, :text)
+  end
+
+    def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+end
